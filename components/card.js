0f32c9db817329793c55f96993924bc865c123a5
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2022-11-15 18:13:39 +0500

    animation

--- components/card.js
+++ components/card.js
@@ -4,7 +4,7 @@ import Image from 'next/image';
 export default function Card({ title, content, image_path }) {
     return (
         <div className='rounded-[32px] relative group transition-all'>
-            <Image src={image_path} alt="Image Missing" className='object-cover w-full h-full rounded-[32px] group-hover:blur' />
+            <Image src={image_path} alt="Image Missing" className='object-cover w-full h-full rounded-[32px] group-hover:blur-[2px]' />
             <div className='absolute bottom-12 md:px-14 px-8'>
                 <h3 className='md:text-[2.5rem] text-[1.5rem] md:leading-[3.1rem] leading-[1.875rem] font-bold -tracking-[1.5px] text-white group-hover:text-white/70 mt-10'>
                     {title}</h3>
