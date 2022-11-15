d0cc8c1b5459f765c6243f44ffbb8ee226c9985b
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2022-11-15 19:52:07 +0500

    animation

--- components/heroSection.js
+++ components/heroSection.js
@@ -56,7 +56,7 @@ export default function HeroSection() {
         </Link>
         <Link
           href="https://pf.kakao.com/_dynxfxj" target="_blank"
-          className="fixed bottom-20 right-5 text-[1rem] leading-[1.1rem] z-50"
+          className="fixed bottom-20 right-5 text-[1rem] leading-[1.1rem] z-50 group transition-all"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -64,7 +64,7 @@ export default function HeroSection() {
             viewBox="0 0 24 24"
             strokeWidth="1.5"
             stroke="currentColor"
-            className="w-16 h-16 bg-white p-4 mb-2 rounded-full hover:scale-75 transition ease-in-out delay-150"
+            className="w-16 h-16 bg-white p-4 mb-2 rounded-full"
           >
             <path
               strokeLinecap="round"
@@ -72,7 +72,7 @@ export default function HeroSection() {
               d="M8.625 9.75a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H8.25m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0H12m4.125 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm0 0h-.375m-13.5 3.01c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.184-4.183a1.14 1.14 0 01.778-.332 48.294 48.294 0 005.83-.498c1.585-.233 2.708-1.626 2.708-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
             />
           </svg>
-          <span>문의하기</span>
+          <span className='group-hover:block hidden'>문의하기</span>
         </Link>
       </div>
     </section>
