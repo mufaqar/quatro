99f08d8af6dd955daf3692e45ec128e1b1e2f641
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2022-11-16 19:57:49 +0500

    fonts

--- components/card.js
+++ components/card.js
@@ -16,7 +16,7 @@ export default function Card({ title, content, image_path, _delay }) {
             <Image src={image_path} alt="Image Missing" className='object-cover w-full h-full rounded-[32px] group-hover:blur-[2px]' />
             <div className='absolute bottom-12 md:px-14 px-8'>
                 <h3
-                    className='md:text-[2.5rem] group-hover:hidden mt-10 text-[1.5rem] md:leading-[3.1rem] leading-[1.875rem] font-bold -tracking-[1.5px] text-white group-hover:text-white/60 '>
+                    className='md:text-[2.5rem] group-hover:hidden mt-10 text-[1.5rem] md:leading-[3.1rem] leading-[1.875rem] -tracking-[1.5px] text-white group-hover:text-white/60 '>
                     <div
                         className=""
                         dangerouslySetInnerHTML={{
@@ -24,15 +24,15 @@ export default function Card({ title, content, image_path, _delay }) {
                         }}
 
                     ></div></h3>
-                <motion.p variants={TextAnimation} initial="offScreen" whileInView="onScreen" transition={{ duration: 0.1 }}
-                    className='md:text-[2.5rem] hidden group-hover:block mt-10 text-[1.5rem] md:leading-[3.1rem] leading-[1.875rem] font-bold -tracking-[1.5px] text-white group-hover:text-white/60 '>
+                <motion.h3 variants={TextAnimation} initial="offScreen" whileInView="onScreen" transition={{ duration: 0.1 }}
+                    className='md:text-[2.5rem] hidden group-hover:block mt-10 text-[1.5rem] md:leading-[3.1rem]  leading-[1.875rem] -tracking-[1.5px] text-white group-hover:text-white/60 '>
                     <div
                         className=""
                         dangerouslySetInnerHTML={{
                             __html: title,
                         }}
 
-                    ></div></motion.p>
+                    ></div></motion.h3>
 
                 <motion.p variants={TextAnimation} initial="offScreen" whileInView="onScreen" transition={{ duration: 0.1 }}
                     className='md:text-[1.25rem] text-[0.9rem] transition-all md:leading-[1.6rem] leading-[1.2rem] -tracking-[1.5px] font-medium text-white mt-5 md:hidden group-hover:block'>
