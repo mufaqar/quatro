4c0a11c87e050a247bcc08b4e8197e669d07e42f
Jaweria Shoukat <jaweeriashoukat@gmail.com>
2022-11-18 18:28:33 +0500

    updated

--- pages/index.js
+++ pages/index.js
@@ -79,7 +79,7 @@ export default function Home(_delay) {
                   한번에 통합관리</h3>
 
                 <p variants={TextAnimation} initial="offScreen" whileInView="onScreen" transition={{ duration: 0.1 }}
-                  className={`md:text-[1.25rem] _text text-[0.9rem] left-1/2 transform -translate-x-1/2 transition-all md:leading-[1.6rem] leading-[1.2rem] -tracking-[1.5px] font-medium mt-4 text-white group-hover:last_item'`}>
+                  className={`md:text-[1.25rem] _text text-[0.9rem] transition-all md:leading-[1.6rem] leading-[1.2rem] -tracking-[1.5px] font-medium mt-4 text-white`}>
                   바이낸스,바이빗,비트겟,업비트 등
                   국내외의 다양한 거래소를 통합하여 한곳에서
                   주문을 관리할수 있어요.</p>
@@ -96,7 +96,7 @@ export default function Home(_delay) {
                   누구든지, 쉽게</h3>
 
                 <p variants={TextAnimation} initial="offScreen" whileInView="onScreen" transition={{ duration: 0.1 }}
-                  className={`md:text-[1.25rem] mx-auto _text text-[0.9rem] text-center transition-all md:leading-[1.6rem] leading-[1.2rem] -tracking-[1.5px] font-medium mt-4 text-black `}>
+                  className={`md:text-[1.25rem] mx-auto _text-left text-center _text text-[0.9rem]  transition-all md:leading-[1.6rem] leading-[1.2rem] -tracking-[1.5px] font-medium mt-4 text-black `}>
                   트레이딩뷰와 연동하여 쉽고 편리하게
                   자동매매 가능한 프로그램을 제공해요.</p>
               </div>
